f0f9fd3beac21dfcf22b796a182e25a714154bb8
phuonganh120705 <phuonganh120705@gmail.com>
2026-04-06 15:36:48 +0700

    Add files via upload

---
+++ hanoiDistrictss.js
@@ -0,0 +1,55 @@
+var hanoiDistricts = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Đông Anh", "Value": 3252, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.85,21.15],[105.86,21.16],[105.87,21.15],[105.85,21.15]]]}
+    },
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Gia Lâm", "Value": 2450, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.90,21.03],[105.91,21.04],[105.92,21.03],[105.90,21.03]]]}
+    },
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Nam Từ Liêm", "Value": 8432, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.75,21.03],[105.76,21.04],[105.77,21.03],[105.75,21.03]]]}
+    },
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Thanh Trì", "Value": 5049, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.85,20.95],[105.86,20.96],[105.87,20.95],[105.85,20.95]]]}
+    },
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Bắc Từ Liêm", "Value": 4882, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.78,21.07],[105.79,21.08],[105.80,21.07],[105.78,21.07]]]}
+    },
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Mê Linh", "Value": 1163, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.70,21.25],[105.71,21.26],[105.72,21.25],[105.70,21.25]]]}
+    },
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Hà Đông", "Value": 11189, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.77,20.96],[105.78,20.97],[105.79,20.96],[105.77,20.96]]]}
+    },
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Sơn Tây", "Value": 584, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.58,21.12],[105.59,21.13],[105.60,21.12],[105.58,21.12]]]}
+    },
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Ba Vì", "Value": 656, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.43,21.05],[105.44,21.06],[105.45,21.05],[105.43,21.05]]]}
+    },
+    {
+      "type": "Feature",
+      "properties": {"Name_2": "Phúc Thọ", "Value": 584, "label_full": "2306060004 Ngô Phương Anh"},
+      "geometry": {"type":"Polygon","coordinates":[[[105.70,21.00],[105.71,21.01],[105.72,21.00],[105.70,21.00]]]}
+    }
+  ]
+};
